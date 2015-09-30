0a061bbd1abda6036db93c266c88a9f70379e852
adam <adamzhangjie@gmail.com>
2015-09-30 20:57:55 +0800

    first commit

---
+++ xpc_1/agent.py
@@ -0,0 +1,11 @@
+# agnet.py
+# !/usr/bin/env python
+# -*- encoding:utf-8 -*-
+import os
+import platform
+
+def getOSInfo():
+	"""To obatin the System information
+	this method used to get OS infromation 
+	"""
+	agentPlatForm = platform.system()
